f6c2a198576ed5cb2df24b3f458ac8f57da74b10
Hsu-Cherng <hsucherng.chong@gmail.com>
2020-03-11 11:10:19 +0800

    Fix JS Partials custom module to work with tab-based indentation

--- dev/custom-modules/metalsmith-js-partial.js
+++ dev/custom-modules/metalsmith-js-partial.js
@@ -21,7 +21,7 @@
 const minimatch = require('minimatch');
 
 module.exports = function(options) {
-    const regexp = /^( +)?\/\/(-)?@js-partial(.+)$/gm;
+    const regexp = /^(\s+)?\/\/(-)?@js-partial(.+)$/gm;
 
     return function(files, metalsmith, done) {
         const jsFiles = Object.keys(files).filter(function(filepath) {
